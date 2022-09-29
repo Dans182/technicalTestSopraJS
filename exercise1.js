378a1cd4ddc2a1ffd2562d76e6052e4542b10921
Daniel Gaiteiro <dgaiteiro@gmail.com>
2022-09-29 18:28:12 +0200

    exercise 1 text

--- exercise1.js
+++ exercise1.js
@@ -11,3 +11,10 @@ const readNumber = () => {
   }
 };
 readNumber();
+
+/*
+Escriba un algoritmo que lea un número entero y determine si es par o impar. 
+Si es par, que escriba todos los pares de manera descendiente desde sí mismo y hasta el cero. 
+Si es impar, que escriba todos los impares de manera descendiente desde si sí mismo hasta el uno. 
+Utilice la instrucción LEER NUMERO al inicio del programa para cargar un número en la variable NUMERO.
+*/
