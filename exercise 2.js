f107cb9d02348b59eff05023080ee2c1374150af
Daniel Gaiteiro <dgaiteiro@gmail.com>
2022-09-30 11:00:30 +0200

    exercise 2 solved

--- exercise 2.js
+++ exercise 2.js
@@ -11,3 +11,48 @@ que actúa como un vector de 50 posiciones.
 Cada elemento de PERSONAS es de un tipo estructurado que dispone dos campos:
 SEXO y EDAD.
 */
+const readPeople = (randomPeople) => {
+  let legalAge = 0;
+  let minor = 0;
+  let legalAgeMan = 0;
+  let minorWoman = 0;
+  let woman = 0;
+
+  for (let i = 0; i < randomPeople.length; i++) {
+    if (randomPeople[i].age >= 18) legalAge += 1;
+  }
+  for (let i = 0; i < randomPeople.length; i++) {
+    if (randomPeople[i].age < 18) minor += 1;
+  }
+  for (let i = 0; i < randomPeople.length; i++) {
+    if (randomPeople[i].age >= 18 && randomPeople[i].sex == "male")
+      legalAgeMan += 1;
+  }
+  for (let i = 0; i < randomPeople.length; i++) {
+    if (randomPeople[i].age < 18 && randomPeople[i].sex == "female")
+      minorWoman += 1;
+  }
+  for (let i = 0; i < randomPeople.length; i++) {
+    if (randomPeople[i].sex == "female") woman += 1;
+  }
+  console.log(randomPeople);
+  console.log(`Hay ${legalAge} personas mayores de edad`);
+  console.log(`Hay ${minor} personas menores de edad`);
+  console.log(`Hay ${legalAgeMan} hombres mayores de edad`);
+  console.log(`Hay ${minorWoman} mujeres menores de edad`);
+  console.log(`El ${(legalAge / 50) * 100}% de las personas son mayores de edad`);
+  console.log(
+    `Las mujeres representan el ${(woman / 50) * 100}% del total de personas`
+  );
+};
+
+let randomPeople = [];
+for (let i = 1; i <= 50; i++) {
+  let sex = ["male", "female"];
+  let randomSex = sex[Math.floor(Math.random() * sex.length)];
+  let randomAge = Math.floor(Math.random() * 100);
+  let people = { sex: randomSex, age: randomAge };
+  randomPeople.push(people);
+}
+
+readPeople(randomPeople);
